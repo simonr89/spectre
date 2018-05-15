4792998f3eaf6def9e42aa094b82437615371c41
Simon Robillard <simon.robillard@chalmers.se>
2018-05-15 11:20:06 +0200

    remove debug in lexer

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
 
 #include "parser/GclParsingContext.hpp"
@@ -12,28 +12,36 @@
 #include "analysis/Properties.hpp"
 #include "program/Program.hpp"
 
-extern FILE* yyin;
+extern FILE *yyin;
+extern bool yy_flex_debug;
 
-void outputUsage() {
+void outputUsage()
+{
     std::cout << "Usage: spectre <filename>" << std::endl;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc <= 1) {
+int main(int argc, char *argv[])
+{
+    if (argc <= 1)
+    {
         outputUsage();
-    } else {
-        if (util::Configuration::instance().setAllValues(argc, argv)) {
+    }
+    else
+    {
+        if (util::Configuration::instance().setAllValues(argc, argv))
+        {
             if (util::Output::initialize())
             {
                 std::string inputFile = argv[argc - 1];
-                
+
                 // test readbility, easier than catching exception thrown by parser
                 std::ifstream istr(inputFile);
-                if (!istr) {
+                if (!istr)
+                {
                     std::cerr << "Unable to read file " << inputFile << std::endl;
                     return 0;
                 }
-                
+
                 // TODO: switch between parsers here
                 std::unique_ptr<program::Program> p;
                 if (true)
@@ -41,17 +49,18 @@ int main(int argc, char *argv[]) {
                     // set input for parser
                     // TODO: remove double checking of file,move setting yyin into parser?
                     const char *fname = inputFile.c_str();
-                    yyin = fopen (fname, "r");
-                    
+                    yy_flex_debug = false;
+                    yyin = fopen(fname, "r");
+
                     // generate a context, whose fields are used as in/out-parameters for parsing
                     parser::GclParsingContext c;
                     c.inputFile = inputFile;
-                    
+
                     // parse the input-program into c
                     parser::GclParser parser(c);
                     parser.set_debug_level(false); // no traces
                     parser.parse();
-                    fclose (yyin);
+                    fclose(yyin);
 
                     if (!c.errorFlag)
                     {
@@ -70,16 +79,15 @@ int main(int argc, char *argv[]) {
                 // run lightweight analysis
                 program::Analyzer a(*p);
                 program::AnalyzerResult aRes = a.computeVariableProperties();
-                
+
                 util::Output::stream() << util::Output::comment;
                 util::Output::stream() << aRes.toString();
                 util::Output::stream() << util::Output::nocomment;
-                
+
                 // create properties and dump them to TPTP
                 program::Properties props(*p, aRes);
                 props.analyze();
                 props.outputTPTP();
-
             }
         }
         return 0;
--- src/parser/GclParsingContext.hpp
+++ src/parser/GclParsingContext.hpp
@@ -11,70 +11,66 @@
 #ifndef __GclParsingContext__
 #define __GclParsingContext__
 
-#include <string>
-#include <vector>
-#include <map>
 #include "Expression.hpp"
 #include "GuardedCommandCollection.hpp"
-#include "Variable.hpp"
 #include "Program.hpp"
+#include "Variable.hpp"
+#include <map>
+#include <string>
+#include <vector>
 
 namespace parser
 {
-    using namespace program;
-    
-    class GclParsingContext
+using namespace program;
+
+class GclParsingContext
+{
+  public:
+    GclParsingContext()
+        : variables(), preconditions(), postconditions(), program(nullptr), negatedPreviousGuards(BooleanExpression::mkConstantBoolean(true)), errorFlag(false), _localScopes()
     {
-    public:
-        
-        GclParsingContext() : variables(), preconditions(), postconditions(), program(nullptr), negatedPreviousGuards(BooleanExpression::mkConstantBoolean(true)), errorFlag(false), _localScopes()  {}
-        
+    }
 
-        /** symbol table */
-        std::map<std::string, PVariable*> variables;
-        std::vector<FExpression*> preconditions;
-        std::vector<FExpression*> postconditions;
-        std::unique_ptr<GuardedCommandCollection> program;
+    /** symbol table */
+    std::map<std::string, PVariable *> variables;
+    std::vector<FExpression *> preconditions;
+    std::vector<FExpression *> postconditions;
+    std::unique_ptr<GuardedCommandCollection> program;
 
-        // given the collection of commands, each guard has the negation
-        // of previous guard added to it so that the guards are exclusive
-        // the conjunction of the guards of all guarded-commands visited until now
-        BooleanExpression* negatedPreviousGuards;
-        std::string inputFile;
+    // given the collection of commands, each guard has the negation
+    // of previous guard added to it so that the guards are exclusive
+    // the conjunction of the guards of all guarded-commands visited until now
+    BooleanExpression *negatedPreviousGuards;
+    std::string inputFile;
 
-        bool errorFlag;
+    bool errorFlag;
 
-        //True if no variable with this name exists in the symbol table
-        bool available(const std::string& name);
-        PVariable* declareVariable(const std::string& name);
-        // looks for local scoped variables first (for quantified formulas) then program variables
-        Variable* getVariable(const std::string& name);
-        
-        void setTypeDeclarationContext(Type t) { _typeDeclCtx = t; }
-        Type typeDeclarationContext() { return _typeDeclCtx; }
-        
-        // Scoped variables are used only in quantified formulas
-        QVariable* openLocalScope(const std::string&, Type t);
-        void closeLocalScope();
-        
-        void printInfo(GuardedCommandCollection &c);
+    //True if no variable with this name exists in the symbol table
+    bool available(const std::string &name);
+    PVariable *declareVariable(const std::string &name);
+    // looks for local scoped variables first (for quantified formulas) then program variables
+    Variable *getVariable(const std::string &name);
 
-        bool containsAssignment(std::pair<FExpression*, std::vector<Assignment*>> assignmentList, Assignment* assignment);
-        void addAdditionalGuards(std::pair<FExpression*, std::vector<Assignment*>> pairGuardsAssignments, Assignment* assignment);
-    protected:
-        
-        /** type of symbols being declared */
-        Type _typeDeclCtx;
-        
-        
-        std::vector<QVariable*> _localScopes;
+    void setTypeDeclarationContext(Type t) { _typeDeclCtx = t; }
+    Type typeDeclarationContext() { return _typeDeclCtx; }
 
-    public:
-        std::unique_ptr<Program> generateProgram();
-    };
-    
-    
-    
+    // Scoped variables are used only in quantified formulas
+    QVariable *openLocalScope(const std::string &, Type t);
+    void closeLocalScope();
+
+    void printInfo(GuardedCommandCollection &c);
+
+    bool containsAssignment(std::pair<FExpression *, std::vector<Assignment *>> assignmentList, Assignment *assignment);
+    void addAdditionalGuards(std::pair<FExpression *, std::vector<Assignment *>> pairGuardsAssignments, Assignment *assignment);
+
+  protected:
+    /** type of symbols being declared */
+    Type _typeDeclCtx;
+
+    std::vector<QVariable *> _localScopes;
+
+  public:
+    std::unique_ptr<Program> generateProgram();
+};
 }
 #endif
-
